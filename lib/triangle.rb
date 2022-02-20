43fbdf6932d6baaeb30f947474b6817a39c2ebad
Nurina Junus <nurina.junus@gmail.com>
2022-02-19 19:31:57 -0700

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_reader :equilateral, :isosceles, :scalene
+  attr_accessor :a, :b, :c
+  
+  def initialize (a,b,c)
+    self.a = a
+    self.b = b
+    self.c = c
+  end
+  
+  def kind
+    if a == 0 || b == 0 || c == 0
+      raise TriangleError
+    elsif (a * b * c).negative?() == true
+      raise TriangleError
+    elsif (a+b) <= c || (a+c) <= b || (b+c) <= a
+      raise TriangleError
+    elsif a == b && b == c
+      :equilateral
+    elsif a == b && c != b || a == c && b != c || b == c && a != c
+      :isosceles
+    elsif (a+b) > c || (a+c) > b || (b+c) > a
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
